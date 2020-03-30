97beb06938794b8ee3dbd07e2a429c27847660c3
Kameshwarang <kamesh0005@gmail.com>
2020-03-30 23:29:04 +0530

    Add files via upload

---
+++ register.php
@@ -0,0 +1,25 @@
+<html>
+<head>
+    <Title>Mak Server</Title>
+</head>
+
+    <body style="font-family: sans-serif">
+        <h1 style="text-align: center"> Registration Form</h1>
+        
+<p style="text-align: center; font-size: 20px; font-weight: bold">
+    <label>UserName</label>
+    <input type="text" name="username" placeholder="Enter The UserName">
+</p>
+        
+<p style="text-align: center; font-size: 20px; font-weight: bold">
+    <label>Email-ID</label>
+    <input type="email" name="email" placeholder="Enter the Email ID">
+</p>
+
+<p style="text-align: center;">
+    <input style="padding:10" type="submit" name="register" value="Register">
+</p>
+        
+    </body>
+    
+</html>
